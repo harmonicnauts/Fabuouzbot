437316dfca3f647116ebe5147dad6d8c6b9507fd
BuildTools <nasutionrachmat@gmail.com>
2024-01-13 19:58:28 +0700

    edit userInfo so that it uses embed

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,4 +1,5 @@
-const { ApplicationCommandOptionType } = require('discord.js');
+
+const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'userinfo',
@@ -13,10 +14,21 @@ module.exports = {
     ],
     execute(interaction, client) {
         const user = interaction.options.getUser('user');
-
+        const userInfoEmbed = new EmbedBuilder()
+            .setColor('#0349fc')
+            .setTitle(`${user.globalName}'s Information`)
+            .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+            .addFields(
+                // { name: '\u200B', value: '\u200B' },
+                { name: 'ID', value: user.id, inline: true },
+                { name: 'Username', value: user.username, inline: true },
+                { name: 'Avatar', value: `[Link](${user.displayAvatarURL({ dynamic: true })})`, inline: true },
+                { name: 'Account Type', value: user.bot ? 'Bot' : 'Human', inline: true },
+            )
+            .setTimestamp()
         interaction.reply({
-            content: `Name: ${user.username}, ID: ${user.id}, Avatar: ${user.displayAvatarURL({ dynamic: true })}`,
-            ephemeral: true,
+            // content: `Name: ${user.username}, ID: ${user.id}, Avatar: ${user.displayAvatarURL({ dynamic: true })}`,
+            embeds: [userInfoEmbed],
         });
     },
 };
