a054a80be72e992cc727ff3fe25a5ebe48cb5579
BuildTools <nasutionrachmat@gmail.com>
2024-01-13 20:50:09 +0700

    improve embed.

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,4 +1,3 @@
-
 const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 
 module.exports = {
@@ -14,21 +13,35 @@ module.exports = {
     ],
     execute(interaction, client) {
         const user = interaction.options.getUser('user');
+        const member = interaction.guild.members.cache.get(user.id);
+
+        const wibOptions = { timeZone: 'Asia/Jakarta' };
+
         const userInfoEmbed = new EmbedBuilder()
             .setColor('#0349fc')
             .setTitle(`${user.globalName}'s Information`)
             .setThumbnail(user.displayAvatarURL({ dynamic: true }))
             .addFields(
-                // { name: '\u200B', value: '\u200B' },
                 { name: 'ID', value: user.id, inline: true },
                 { name: 'Username', value: user.username, inline: true },
                 { name: 'Avatar', value: `[Link](${user.displayAvatarURL({ dynamic: true })})`, inline: true },
                 { name: 'Account Type', value: user.bot ? 'Bot' : 'Human', inline: true },
+                { name: 'Account Created At', value: `${user.createdAt.toLocaleString('id-ID', wibOptions)} WIB`, inline: true },
+                { name: 'Account Age', value: calculateAge(user.createdAt), inline: true },
+                { name: 'Joined Server At', value: `${member.joinedAt.toLocaleString('id-ID', wibOptions)} WIB`, inline: true },
+                { name: 'Joined Server Age', value: calculateAge(member.joinedAt), inline: true },
             )
-            .setTimestamp()
+            .setTimestamp();
+
         interaction.reply({
-            // content: `Name: ${user.username}, ID: ${user.id}, Avatar: ${user.displayAvatarURL({ dynamic: true })}`,
             embeds: [userInfoEmbed],
         });
     },
 };
+
+// Function to calculate age
+function calculateAge(date) {
+    const ageMilliseconds = Date.now() - date;
+    const age = new Date(ageMilliseconds);
+    return `${age.getUTCFullYear() - 1970} years, ${age.getUTCMonth()} months, ${age.getUTCDate()} days`;
+}
