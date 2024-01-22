0f1d1d101c2be59776e84631999296c31d40ce00
BuildTools <nasutionrachmat@gmail.com>
2024-01-22 16:06:59 +0700

    rapihin

--- services/fetchWeatherData.js
+++ services/fetchWeatherData.js
@@ -1,5 +1,55 @@
 var convert = require('xml-js');
 
+///////////////////////////////////////////////////////////////////////////////////////
+// Parameter (key)
+// Waktu dalam UTC-YYYYMMDDHHmmss (key id: timerange)
+// Cuaca berupa kode cuaca (key id: weather)
+// Suhu Udara dalam °C dan °F (key id: t)
+// Suhu Udara Minimum dalam °C dan °F (key id: tmin)
+// Suhu Udara Maksimum dalam °C dan °F (key id: tmax)
+// Kelembapan Udara dalam % (key id: hu)
+// Kelembapan Udara Minimum dalam % (key id: humin)
+// Kelembapan Udara Maksimum dalam % (key id: humax)
+// Kecepatan Angin dalam knot, mph, kph, dan ms (key id: ws)
+// Arah Angin dalam derajat, CARD, dan SEXA (key id: wd)
+
+// Kode Cuaca
+// 0 : Cerah / Clear Skies
+// 1 : Cerah Berawan / Partly Cloudy
+// 2 : Cerah Berawan / Partly Cloudy
+// 3 : Berawan / Mostly Cloudy
+// 4 : Berawan Tebal / Overcast
+// 5 : Udara Kabur / Haze
+// 10 : Asap / Smoke
+// 45 : Kabut / Fog
+// 60 : Hujan Ringan / Light Rain
+// 61 : Hujan Sedang / Rain
+// 63 : Hujan Lebat / Heavy Rain
+// 80 : Hujan Lokal / Isolated Shower
+// 95 : Hujan Petir / Severe Thunderstorm
+// 97 : Hujan Petir / Severe Thunderstorm
+
+// Kode Arah Angin (CARD) (dibaca: dari arah ...)
+// N (North)
+// NNE (North-Northeast)
+// NE (Northeast)
+// ENE (East-Northeast)
+// E (East)
+// ESE (East-Southeast)
+// SE (Southeast)
+// SSE (South-Southeast)
+// S (South)
+// SSW (South-Southwest)
+// SW (Southwest)
+// WSW (West-Southwest)
+// W (West)
+// WNW (West-Northwest)
+// NW (Northwest)
+// NNW (North-Northwest)
+// VARIABLE (berubah-ubah)
+///////////////////////////////////////////////////////////////////////////////////////
+
+
 async function fetchWeatherData(selectedLocation, selectedParam) {
   try {
     console.log(selectedLocation);
